1117c5b433111eb6885bb14f4055e2a852112a7a
Samuel Tan <126168312+SAMTAN444@users.noreply.github.com>
2026-07-06 14:33:17 +0800

    Add: lru-cache - submission-11

---
+++ Data Structures & Algorithms/lru-cache/submission-11.py
@@ -0,0 +1,27 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = []
+
+    def get(self, key: int) -> int:
+        for i in range(len(self.cache)):
+            if key == self.cache[i][0]:
+                temp = self.cache.pop(i)
+                val = temp[1]
+                self.cache.append(temp)
+                return val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        for i in range(len(self.cache)):
+            if key == self.cache[i][0]:
+                temp = self.cache.pop(i)
+                temp[1] = value
+                self.cache.append(temp)
+                return
+        
+        if self.capacity == len(self.cache):
+            self.cache.pop(0)
+        
+        self.cache.append([key, value])
